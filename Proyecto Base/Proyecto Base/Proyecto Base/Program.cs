fd1d635e82f651e3d15abba9e20d4e8d0216cfcb
laureanoan0 <laureanomeyer22@gmail.com>
2025-04-21 13:48:07 -0300

    Add files via upload

---
+++ Proyecto Base/Proyecto Base/Proyecto Base/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+using Tao.Sdl;
+
+
+
+namespace MyGame
+{
+
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Engine.Initialize();
+
+            while (true)
+            {   
+                
+            }
+        }
+    }
+}
