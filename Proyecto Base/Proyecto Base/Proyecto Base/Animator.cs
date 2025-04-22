fdc632742cf872015cdb615e45dbef750a0b7ceb
Fakunesu <facundo.minones1401@gmail.com>
2025-04-22 00:56:01 -0300

    Fakunesu12345

--- Proyecto Base/Proyecto Base/Proyecto Base/Animator.cs
+++ Proyecto Base/Proyecto Base/Proyecto Base/Animator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,42 @@ namespace MyGame
 {
     public class Animator
     {
+        private string name;
+        private List<Image> images;
+        private float speedAnimation;
+        private bool isLooping;
+        private int currentFrame;
+        private float currentTime;
 
+        public Animator(string name, List<Image> images, float speedAnimation, bool isLooping)
+        {
+            this.name = name;   
+            this.images = images;   
+            this.speedAnimation = speedAnimation;   
+            this.isLooping = isLooping; 
+        }
 
+        public void Update()
+        {
+        currentTime += Program.deltaTime;
 
+            if (currentTime > speedAnimation)
+            {
+            currentFrame++;
+            currentTime = 0;
+
+                if (currentFrame >= images.Count)
+                {
+                    if (isLooping)
+                    {
+                        currentFrame = 0;
+                    }
+                    else
+                    {
+                        currentFrame--;
+                    }
+                }
+            }
+    }
     }
 }
