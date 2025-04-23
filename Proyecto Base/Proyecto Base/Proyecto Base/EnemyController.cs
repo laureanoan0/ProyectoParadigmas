260fa1cd74f439590f1227fb30f90f1ec9aba0f6
alu765 <alu765@laboratorios.local>
2025-04-23 16:36:12 -0300

    facuforro

---
+++ Proyecto Base/Proyecto Base/Proyecto Base/EnemyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    public class EnemyController
+    {
+
+        private Transform enemyTransform;
+        public EnemyController(Transform enemyTransform)
+        {
+            this.enemyTransform = enemyTransform;
+
+        }
+
+    }
+}
